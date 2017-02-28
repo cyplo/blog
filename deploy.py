cfe78aa4d7371e4316fcf975727803c86d7aa186
Cyryl Płotnicki <cyplo@cyplo.net>
2017-02-28 15:59:52 +0000

    Support for continuing interrupted upload

--- deploy.py
+++ deploy.py
@@ -6,16 +6,19 @@ import requests
 import sys
 import hashlib
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 3 and len(sys.argv) != 4:
     print('usage: ')
-    print(sys.argv[0] + " netlify_site_id directory_to_deploy")
+    print(sys.argv[0] + " netlify_site_id directory_to_deploy [deployment_id_to_continue]")
     sys.exit(1)
 auth_token = os.getenv("NETLIFY_TOKEN") 
 if not auth_token:
     print('Please set NETLIFY_TOKEN')
-    sys.exit(3)
+    sys.exit(2)
 site_id = sys.argv[1]
 directory_to_deploy = os.path.abspath(sys.argv[2])
+if len(sys.argv) == 4:
+    deployment_id = sys.argv[3]
+
 print("Deploying " + directory_to_deploy + " to Netlify as " + site_id)
 directory_to_hash = directory_to_deploy
 paths_to_hash = []
@@ -29,7 +32,7 @@ if os.path.isfile(os.path.join(directory_to_hash, index_filename)):
     print('OK')
 else:
     print('No '+index_filename)
-    sys.exit(2)
+    sys.exit(3)
     
 print('Hashing...', end="")
 file_hashes = {}
@@ -50,22 +53,34 @@ for path in paths_to_hash:
         file_hashes[relative_path] = file_hash
 print('OK')
 
-print('Creating new deployment...', end="")
-
 json_headers = {"Authorization":"Bearer " + auth_token, 'content-type': "application/json"}
 base_url = 'https://api.netlify.com/api/v1/'
 site_url = base_url + "sites/" + site_id + "/"
-new_deploy_url = site_url + "deploys/"
-new_deploy = {'files' : file_hashes, 'context': 'deploy-preview', 'draft': True}
-new_deploy_json = json.dumps(new_deploy)
-response = requests.post(url=new_deploy_url, headers=json_headers, data=new_deploy_json)
+deploys_url = site_url + "deploys/"
+response = {}
 
-if not response.ok:
-    print('Failed with error')
-    print(response.content)
-    sys.exit(3)
+if not deployment_id:
+    print('Creating new deployment...', end="")
 
-deployment_id = response.json()['id']
+    new_deploy = {'files' : file_hashes, 'context': 'deploy-preview', 'draft': True}
+    new_deploy_json = json.dumps(new_deploy)
+    response = requests.post(url=deploys_url, headers=json_headers, data=new_deploy_json)
+
+    if not response.ok:
+        print('Failed with error')
+        print(response.content)
+        sys.exit(4)
+
+    deployment_id = response.json()['id']
+else:
+    print('Getting deployment information...', end="")
+    deploy_url = deploys_url + deployment_id + "/"
+    response = requests.get(url=deploy_url, headers=json_headers)
+    if not response.ok:
+        print('Failed with error')
+        print(response.content)
+        sys.exit(5)
+
 print(deployment_id)
 
 required_file_hashes = response.json()['required']
@@ -77,13 +92,13 @@ file_upload_headers = {"Authorization":"Bearer " + auth_token, 'content-type': "
 for required_hash in required_file_hashes:
     current_file_name = files_for_hashes[required_hash]
     print('Uploading ' + current_file_name + "...", end="")
-    with open('output/'+current_file_name, 'rb') as current_file_handle:
+    with open(os.path.join(directory_to_deploy, current_file_name), 'rb') as current_file_handle:
         file_upload_url = new_deploy_url + "files/" + current_file_name
         response = requests.put(url=file_upload_url, headers=file_upload_headers, data=current_file_handle)
         if not response.ok:
             print('Failed with error')
             print(response.content)
-            sys.exit(4)
+            sys.exit(6)
         else:
             print('OK')
 
