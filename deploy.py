503d1d6531f57c1fba765640cd210fc1eb237ecf
Cyryl Płotnicki <cyplo@cyplo.net>
2017-02-28 17:49:56 +0000

    Extract out functions in deployment script

--- deploy.py
+++ deploy.py
@@ -6,71 +6,76 @@ import requests
 import sys
 import hashlib
 
-if len(sys.argv) != 3 and len(sys.argv) != 4:
-    print('usage: ')
-    print(sys.argv[0] + " netlify_site_id directory_to_deploy [deployment_id_to_continue]")
-    sys.exit(1)
-auth_token = os.getenv("NETLIFY_TOKEN") 
-if not auth_token:
-    print('Please set NETLIFY_TOKEN')
-    sys.exit(2)
-site_id = sys.argv[1]
-directory_to_deploy = os.path.abspath(sys.argv[2])
 
-print("Deploying " + directory_to_deploy + " to Netlify as " + site_id)
-directory_to_hash = directory_to_deploy
-paths_to_hash = []
+def checkArguments():
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print('usage: ')
+        print(sys.argv[0] + " netlify_site_id directory_to_deploy [deployment_id_to_continue]")
+        sys.exit(1)
 
-for root, dirs, filenames in os.walk(directory_to_hash, topdown=False):
-    paths_to_hash += ((os.path.join(root, name)) for name in filenames)
 
-print('Validating structure...', end="")
-index_filename = "index.html"
-if os.path.isfile(os.path.join(directory_to_hash, index_filename)):
+def getAuthToken():
+    token = os.getenv("NETLIFY_TOKEN")
+    if not token:
+        print('Please set NETLIFY_TOKEN')
+        sys.exit(2)
+
+    return token
+
+def getDirectoryToDeploy():
+    return os.path.abspath(sys.argv[2])
+
+def getSiteId():
+    return sys.argv[1]
+
+def validateDirectoryStructure():
+    print('Validating structure...', end="")
+    index_filename = "index.html"
+    if os.path.isfile(os.path.join(directory_to_deploy, index_filename)):
+        print('OK')
+    else:
+        print('No ' + index_filename)
+        sys.exit(3)
+
+def getAllFilePathsForDirectory(directory):
+    paths = []
+    for root, dirs, filenames in os.walk(directory, topdown=False):
+        paths += ((os.path.join(root, name)) for name in filenames)
+    return paths
+
+def calculateHashesForPaths(paths):
+    print('Hashing...', end="")
+    hashes = {}
+    for path in paths:
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+        sha1 = hashlib.sha1()
+
+        with open(path, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+
+            file_hash = sha1.hexdigest()
+            relative_path = path.replace(directory_to_deploy, '')
+            hashes[relative_path] = file_hash
     print('OK')
-else:
-    print('No '+index_filename)
-    sys.exit(3)
-    
-print('Hashing...', end="")
-file_hashes = {}
-for path in paths_to_hash:
-    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+    return hashes
 
-    sha1 = hashlib.sha1()
-
-    with open(path, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha1.update(data)
-
-        file_hash = sha1.hexdigest()
-        relative_path=path.replace(directory_to_deploy, '')
-        file_hashes[relative_path] = file_hash
-print('OK')
-
-json_headers = {"Authorization":"Bearer " + auth_token, 'content-type': "application/json"}
-base_url = 'https://api.netlify.com/api/v1/'
-site_url = base_url + "sites/" + site_id + "/"
-deploys_url = site_url + "deploys/"
-response = {}
-
-if not len(sys.argv) == 4:
+def createDeployment():
     print('Creating new deployment...', end="")
-
-    new_deploy = {'files' : file_hashes, 'context': 'deploy-preview', 'draft': True}
+    new_deploy = {'files': file_hashes, 'context': 'deploy-preview', 'draft': True}
     new_deploy_json = json.dumps(new_deploy)
     response = requests.post(url=deploys_url, headers=json_headers, data=new_deploy_json)
-
     if not response.ok:
         print('Failed with error')
         print(response.content)
         sys.exit(4)
+    return response.json()
 
-    deployment_id = response.json()['id']
-else:
+def getExistingDeployment():
     print('Getting deployment information...', end="")
     deployment_id = sys.argv[3]
     deploy_url = deploys_url + deployment_id + "/"
@@ -79,19 +84,45 @@ else:
         print('Failed with error')
         print(response.content)
         sys.exit(5)
+    return response.json()
 
+def invertDict(dict):
+    return {v: k for k, v in dict.items()}
+
+checkArguments()
+auth_token = getAuthToken()
+site_id = getSiteId()
+json_headers = {"Authorization": "Bearer " + auth_token, 'content-type': "application/json"}
+base_url = 'https://api.netlify.com/api/v1/'
+site_url = base_url + "sites/" + site_id + "/"
+deploys_url = site_url + "deploys/"
+
+directory_to_deploy = getDirectoryToDeploy()
+
+print("Deploying " + directory_to_deploy + " to Netlify as " + site_id)
+validateDirectoryStructure()
+
+paths_to_hash = getAllFilePathsForDirectory(directory_to_deploy)
+file_hashes = calculateHashesForPaths(paths_to_hash)
+
+if not len(sys.argv) == 4:
+    deployment = createDeployment()
+
+else:
+    deployment = getExistingDeployment()
+
+deployment_id = deployment['id']
 print(deployment_id)
 
-required_file_hashes = response.json()['required']
-files_for_hashes = {v: k for k, v in file_hashes.items()}
+files_for_hashes = invertDict(file_hashes)
 
 new_deploy_url = base_url + "deploys/" + deployment_id + "/"
-file_upload_headers = {"Authorization":"Bearer " + auth_token, 'content-type': "application/octet-stream"}
+file_upload_headers = {"Authorization": "Bearer " + auth_token, 'content-type': "application/octet-stream"}
 
-for required_hash in required_file_hashes:
+for required_hash in deployment['required']:
     current_file_name = files_for_hashes[required_hash]
     print('Uploading ' + current_file_name + "...", end="")
-    with open(os.path.join(directory_to_deploy, "./"+current_file_name), 'rb') as current_file_handle:
+    with open(os.path.join(directory_to_deploy, "./" + current_file_name), 'rb') as current_file_handle:
         file_upload_url = new_deploy_url + "files/" + current_file_name
         response = requests.put(url=file_upload_url, headers=file_upload_headers, data=current_file_handle)
         if not response.ok:
@@ -101,6 +132,4 @@ for required_hash in required_file_hashes:
         else:
             print('OK')
 
-print('DONE')
-
-
+print('Done uploading, waiting for the confirmation...')
