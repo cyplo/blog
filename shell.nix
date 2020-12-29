e30688788ba33f748fa34dfd0624d5eb4cb794c0
Cyryl Płotnicki <cyplo@cyplo.net>
2020-12-29 13:32:27 +0000

    more stable dev shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,22 @@
 let
   pkgs = import <nixpkgs> {};
+  netlify-deployer = pkgs.python38Packages.buildPythonPackage rec {
+    pname = "netlify_deployer";
+    version = "0.5.2";
+
+    src = pkgs.python38Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "aae0092b36e7408281ad73269b446c701edaacecc8ba1a07cc85671e3ddfae6e";
+    };
+
+    propagatedBuildInputs = with pkgs; with pkgs.python38Packages; [ requests git pbr wheel setuptools  ];
+
+    doCheck = false;
+  };
+
+  customPython = pkgs.python38.buildEnv.override {
+    extraLibs = [ netlify-deployer ];
+  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; with pkgs.python38Packages; [
@@ -9,11 +26,6 @@ pkgs.mkShell {
       requests
       pip
       virtualenv
+      customPython
   ];
-
-  shellHook = ''
-    virtualenv simpleEnv
-    source simpleEnv/bin/activate
-    python3 -m pip install netlify-deployer
-    '';
 }
