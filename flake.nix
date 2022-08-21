0c858a826dd440731d971d9068c5ba46cb5e0937
Cyryl Płotnicki <cyplo@cyplo.dev>
2022-08-21 11:41:21 +0100

    don't publish to netlify

--- flake.nix
+++ flake.nix
@@ -10,45 +10,15 @@
 
   outputs = { self, nixpkgs, utils, flake-compat }:
     utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        netlify-deployer = pkgs.python38Packages.buildPythonPackage rec {
-          pname = "netlify_deployer";
-          version = "0.5.2";
-
-          src = pkgs.python38Packages.fetchPypi {
-            inherit pname version;
-            sha256 =
-              "aae0092b36e7408281ad73269b446c701edaacecc8ba1a07cc85671e3ddfae6e";
-          };
-
-          propagatedBuildInputs = with pkgs;
-            with pkgs.python38Packages; [
-              requests
-              git
-              pbr
-              wheel
-              setuptools
-            ];
-
-          doCheck = false;
-        };
-
-        customPython =
-          pkgs.python38.buildEnv.override { extraLibs = [ netlify-deployer ]; };
+      let pkgs = nixpkgs.legacyPackages."${system}";
       in rec {
         # `nix develop`
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs;
             with pkgs.python38Packages; [
               cacert
-              customPython
               git
               hugo
-              hut
-              pip
-              requests
-              virtualenv
             ];
         };
       });
